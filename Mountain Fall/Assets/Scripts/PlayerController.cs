8942332ca2265491cefabdade2c546cf90ec78e8
Merlan <merlan01tel@gmail.com>
2023-02-12 14:16:59 +0600

    Update #3
    Added Player Mesh and Material

--- Mountain Fall/Assets/Scripts/PlayerController.cs
+++ Mountain Fall/Assets/Scripts/PlayerController.cs
@@ -4,86 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public static PlayerController instance;
-
-    public enum Direction { Left, Right, Down, Up};
-
-    bool[] swipe = new bool[4];
-
-    Vector2 startTouch;
-    bool touchMoved;
-    Vector2 swipeDelta;
-
-    const float SWIPE_THRESHOLD = 50;
-
-    public delegate void MoveDelegate(bool[] swipes);
-    public MoveDelegate MoveEvent;
-
-    public delegate void ClickDelegate(Vector2 pos);
-    public ClickDelegate ClickEvent;
-
-    Vector2 TouchPosition() { return (Vector2)Input.mousePosition; }
-    bool TouchBegan() { return Input.GetMouseButtonDown(0); }
-    bool TouchEnded() { return Input.GetMouseButtonUp(0); }
-    bool GetTouch() { return Input.GetMouseButton(0); }
-
-    private void Awake()
-    {
-        instance = this;
-    }
+    public GameObject Player;
+    public float speed = 2f;
 
     void Update()
     {
-        if (TouchBegan())
+        if (Input.GetKey(KeyCode.D))
         {
-            startTouch = TouchPosition();
-            touchMoved = true;
+            Player.transform.Translate(speed * Time.deltaTime, 0, 0);
         }
-        else if (TouchEnded() && touchMoved == false)
+        if (Input.GetKey(KeyCode.A))
         {
-            SendSwipe();
-            touchMoved = false;
-        }
-
-        swipeDelta = Vector2.zero;
-        if (touchMoved && GetTouch())
-        {
-            swipeDelta = TouchPosition() - startTouch;
-        }
-
-        if (swipeDelta.magnitude > SWIPE_THRESHOLD)
-        {
-            if (Mathf.Abs(swipeDelta.x) > Mathf.Abs(swipeDelta.y))
-            {
-                swipe[(int)Direction.Left] = swipeDelta.x < 0;
-                swipe[(int)Direction.Right] = swipeDelta.x > 0;
-            } else
-            {
-                swipe[(int)Direction.Up] = swipeDelta.y < 0;
-                swipe[(int)Direction.Down] = swipeDelta.y < 0;
-            }
-            SendSwipe();
+            Player.transform.Translate(-speed * Time.deltaTime, 0, 0);
         }
     }
 
-    void SendSwipe()
-    {
-        if (swipe[0] || swipe[1] || swipe[2] || swipe[3])
-        {
-            Debug.Log(swipe[0] + "|" + swipe[1] + "|" + swipe[2] + "|" + swipe[3]);
-            MoveEvent?.Invoke(swipe);
-        }
-        else
-        {
-            Debug.Log("Click");
-            ClickEvent?.Invoke(TouchPosition());
-        }
-        Reset();
-    }
-    private void Reset()
-    {
-        startTouch = swipeDelta = Vector2.zero;
-        touchMoved = false;
-        for(int i = 0; i < 4; i++) { swipe[i] = false; }
-    }
 }
